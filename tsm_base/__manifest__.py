45961afb5cf2fa60e511dc1c541ca9ec3f27202d
bilbonet.net <bilbonet@odoo-11.bilbonet.net>
2018-11-11 22:02:58 +0100

    module tsm_base

---
+++ tsm_base/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2017 Jesus Ramiro <jesus@bilbonet.net>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Technical Support Management',
+    'version': '11.0.1.0.0',
+    'category': 'Management',
+    'license': 'AGPL-3',
+    'author': 'Jesus Ramiro (Bilbonet.NET)',
+    'website': 'https://www.bilbonet.net',
+    'depends': [
+        'base_setup',
+        'product',
+        'mail',
+        'web',
+    ],
+    'data': [
+        'security/tsm_security.xml',
+        'security/ir.model.access.csv',
+        'views/tsm_project_views.xml',
+        'views/tsm_task_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+}
