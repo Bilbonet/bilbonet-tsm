40951163b42a970ed5b58cc8cdb9b693aa78da84
bilbonet.net <bilbonet@odoo-11.bilbonet.net>
2019-03-06 14:04:27 +0100

    add option to send task by mail

--- tsm_base/models/tsm_task.py
+++ tsm_base/models/tsm_task.py
@@ -178,6 +178,34 @@ class TsmTask(models.Model):
         if self.user_id:
             self.date_start = fields.Datetime.now()
 
+    @api.multi
+    def action_task_send(self):
+        self.ensure_one()
+        template = self.env.ref(
+            'tsm_base.tsm_task_email_template',
+            False,
+        )
+        compose_form = self.env.ref('mail.email_compose_message_wizard_form',
+                                    False)
+        ctx = dict(
+            default_model='tsm.task',
+            default_res_id=self.id,
+            default_use_template=bool(template),
+            default_template_id=template and template.id or False,
+            default_composition_mode='comment',
+        )
+        return {
+            'name': _('Compose Email'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+            'context': ctx,
+        }
+
     # ------------------------------------------------
     # CRUD overrides
     # ------------------------------------------------
