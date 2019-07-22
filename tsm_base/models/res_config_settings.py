ff8efb718c23c8cadd90ed7745452bf49c0c91d4
bilbonet.net <bilbonet@odoo-casa.bilbonet.net>
2019-07-22 22:15:06 +0200

    Change the settings behavior

--- tsm_base/models/res_config_settings.py
+++ tsm_base/models/res_config_settings.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Jesus Ramiro <jesus@bilbonet.net>
+# Copyright 2019 Jesus Ramiro <jesus@bilbonet.net>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -7,19 +7,12 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    tsm_tags_in_task = fields.Boolean(string="Use Tags in Tasks")
+    default_tags_in_task = fields.Boolean(string="Use Tags in Tasks",
+                                          default_model='tsm.task')
 
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            tsm_tags_in_task=self.env['ir.config_parameter'].sudo().get_param(
-                'tsm_base.tsm_tags_in_task')
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param(
-            'tsm_base.tsm_tags_in_task', self.tsm_tags_in_task)
+    # Update field in active tasks
+    @api.onchange('default_tags_in_task')
+    def _onchange_default_tags_in_task(self):
+        self.env['tsm.task'].search([
+            ('active', '=', True)
+        ]).write({'tags_in_task': self.default_tags_in_task})
