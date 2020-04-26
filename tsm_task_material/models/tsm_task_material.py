621aeac8d5831c2c52dfa927fe9e5b4a8e79babe
bilbonet.net <bilbonet@odoo-casa.bilbonet.net>
2020-04-26 21:27:29 +0200

    [FIX] little changes in settings view

--- tsm_task_material/models/tsm_task_material.py
+++ tsm_task_material/models/tsm_task_material.py
@@ -1,7 +1,7 @@
 # Copyright 2018 - Bilbonet <jesus@bilbonet.net>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 import odoo.addons.decimal_precision as dp
 
