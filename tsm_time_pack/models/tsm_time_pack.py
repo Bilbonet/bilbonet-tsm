6471139226765cfd45275f221039cc1178102203
bilbonet <jesus@bilbonet.net>
2023-09-30 22:10:08 +0200

    [MIG] Mail template for sending time pack resum

--- tsm_time_pack/models/tsm_time_pack.py
+++ tsm_time_pack/models/tsm_time_pack.py
@@ -200,35 +200,26 @@ class TsmTimePack(models.Model):
         with the time pack template message loaded by default
         '''
         self.ensure_one()
-        ir_model_data = self.env['ir.model.data']
-        try:
-            template_id = ir_model_data.get_object_reference('tsm_time_pack', 'tsm_time_pack_email_template')[1]
-        except ValueError:
-            template_id = False
-        try:
-            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
-        except ValueError:
-            compose_form_id = False
         lang = self.env.context.get('lang')
-        template = template_id and self.env['mail.template'].browse(template_id)
-        if template and template.lang:
-            lang = template._render_template(template.lang, 'tsm.time.pack', self.ids[0])
+        mail_template = self.env.ref('tsm_time_pack.tsm_time_pack_email_template', raise_if_not_found=False)
+        if mail_template and mail_template.lang:
+            lang = mail_template._render_lang(self.ids)[self.id]
         ctx = {
             'default_model': 'tsm.time.pack',
-            'default_res_id': self.ids[0],
-            'default_use_template': bool(template_id),
-            'default_template_id': template_id,
+            'default_res_id': self.id,
+            'default_use_template': bool(mail_template),
+            'default_template_id': mail_template.id if mail_template else None,
             'default_composition_mode': 'comment',
-            'model_description': self.with_context(lang=lang).name,
-            'force_email': True
+            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+            'force_email': True,
+            'model_description': 'TSM Time Pack',
         }
         return {
             'type': 'ir.actions.act_window',
-            'view_type': 'form',
             'view_mode': 'form',
             'res_model': 'mail.compose.message',
-            'views': [(compose_form_id, 'form')],
-            'view_id': compose_form_id,
+            'views': [(False, 'form')],
+            'view_id': False,
             'target': 'new',
             'context': ctx,
         }
