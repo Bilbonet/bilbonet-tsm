b125352eb30d3fff45adade5f2dc616ca64bcca7
bilbonet <jesus@bilbonet.net>
2023-10-04 18:17:55 +0200

    [MIG] tsm_base from v16

--- tsm_base/__manifest__.py
+++ tsm_base/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Technical Support Management',
-    'version': '16.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Management',
     'license': 'AGPL-3',
     'author': 'Jesus Ramiro (Bilbonet.NET)',
