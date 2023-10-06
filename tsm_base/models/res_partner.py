539c461fc57af9dda405a1d4dfa8122d71729397
bilbonet <jesus@bilbonet.net>
2023-10-06 18:29:31 +0200

    [FIX] function _compute_tsm_task_count in res.partner

--- tsm_base/models/res_partner.py
+++ tsm_base/models/res_partner.py
@@ -11,23 +11,30 @@ class ResPartner(models.Model):
         comodel_name='tsm.task', 
         inverse_name='partner_id',
        )
-    tsm_task_count = fields.Integer(string='Amount Partner Tasks',
-        compute='_compute_tsm_task_count')
+    tsm_task_count = fields.Integer(
+        string='Amount Partner Tasks',
+        compute='_compute_tsm_task_count',
+        readonly=True,
+    )
 
     def _compute_tsm_task_count(self):
         # retrieve all children partners and prefetch 'parent_id' on them
         all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
         all_partners.read(['parent_id'])
 
-        task_data = self.env['tsm.task']._read_group(
+        tsm_task_groups = self.with_context(
+            active_test=False
+        ).env['tsm.task'].read_group(
             domain=[('partner_id', 'in', all_partners.ids)],
             fields=['partner_id'], groupby=['partner_id']
         )
 
-        self.tsm_task_count = 0
-        for group in task_data:
+        partners = self.browse()
+        for group in tsm_task_groups:
             partner = self.browse(group['partner_id'][0])
             while partner:
                 if partner in self:
                     partner.tsm_task_count += group['partner_id_count']
+                    partners |= partner
                 partner = partner.parent_id
+        (self - partners).tsm_task_count = 0
