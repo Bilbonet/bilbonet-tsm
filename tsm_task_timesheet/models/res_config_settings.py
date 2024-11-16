3024d6b53f925627d9b6b3555ca7644c778fb631
bilbonet <jesus@bilbonet.net>
2024-11-16 21:01:56 +0100

    [IMP] timesheet set name and avoid have title and tags

--- tsm_task_timesheet/models/res_config_settings.py
+++ tsm_task_timesheet/models/res_config_settings.py
@@ -1,12 +1,26 @@
 # Copyright 2019 Jesus Ramiro <jesus@bilbonet.net>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    group_tsm_timesheet_title = fields.Boolean("Timesheet Title",
-                implied_group='tsm_task_timesheet.group_tsm_timesheet_title')
-    group_tsm_timesheet_tag = fields.Boolean("Timesheet Tag",
-                implied_group='tsm_task_timesheet.group_tsm_timesheet_tag')
+    group_tsm_timesheet_title = fields.Boolean(
+        string="Timesheet Title",
+        implied_group="tsm_task_timesheet.group_tsm_timesheet_title",
+    )
+    group_tsm_timesheet_tag = fields.Boolean(
+        string="Timesheet Tag",
+        implied_group="tsm_task_timesheet.group_tsm_timesheet_tag",
+    )
+
+    @api.onchange("group_tsm_timesheet_title")
+    def onchange_group_tsm_timesheet_title(self):
+        if self.group_tsm_timesheet_title:
+            self.group_tsm_timesheet_tag = False
+            
+    @api.onchange("group_tsm_timesheet_tag")
+    def onchange_group_tsm_timesheet_tag(self):
+        if self.group_tsm_timesheet_tag:
+            self.group_tsm_timesheet_title = False
--- tsm_task_timesheet/models/tsm_task_timesheet.py
+++ tsm_task_timesheet/models/tsm_task_timesheet.py
@@ -6,67 +6,90 @@ from odoo.exceptions import ValidationError
 
 
 class TsmTaskTimesheet(models.Model):
-    _name = 'tsm.task.timesheet'
-    _description = 'Spent time in tasks'
-    _order = 'date_time desc, id desc'
+    _name = "tsm.task.timesheet"
+    _description = "Spent time in tasks"
+    _order = "date_time desc, id desc"
+
+    @api.depends("tag_ids")
+    def _compute_default_name(self):
+        for ts in self:
+            if self.user_has_groups("tsm_task_timesheet.group_tsm_timesheet_title"):
+                ts.name = "/"
+            if self.user_has_groups("tsm_task_timesheet.group_tsm_timesheet_tag"):
+                if ts.tag_ids:
+                    ts.name = ts.tag_ids.name
 
     def _get_default_tag_id(self):
-        """ Assign Default Tag if exists """
-        tag_id = self.env['tsm.task.timesheet.tags'].search([
-            ('default', '=', True)], limit=1)
-        if not tag_id:
-            return False
-        return tag_id.id
+        if self.user_has_groups("tsm_task_timesheet.group_tsm_timesheet_tag"):
+            tag_id = self.env["tsm.task.timesheet.tags"].search(
+                [("default", "=", True)], limit=1
+            )
+            if tag_id:
+                return tag_id.id
+        return False
 
+    name = fields.Char(
+        string="Timesheet Title",
+        compute=_compute_default_name,
+        readonly=False,
+        store=True,
+        required=True,
+    )
+    tag_ids = fields.Many2one(
+        comodel_name="tsm.task.timesheet.tags",
+        string="Timesheet Tags",
+        default=_get_default_tag_id,
+    )
     company_id = fields.Many2one(
-        comodel_name='res.company', 
-        string='Company',
+        comodel_name="res.company",
+        string="Company",
         default=lambda self: self.env.user.company_id,
         required=True,
     )
-    active = fields.Boolean(string='Active', default=True,
-        help="If the active field is set to False, it will allow "
-             "you to hide the account without removing it.",
-    )
-    date_time = fields.Datetime(string='Date', default=fields.Datetime.now)
-    name = fields.Char(string='Timesheet Title', default="/", required=True)
-    amount = fields.Float(string='Quantity', default=0.0)
+    # active = fields.Boolean(
+    #     string="Active",
+    #     default=True,
+    #     help="If the active field is set to False, it will allow "
+    #     "you to hide the account without removing it.",
+    # )
+    date_time = fields.Datetime(string="Date", default=fields.Datetime.now)
+    amount = fields.Float(string="Quantity", default=0.0)
     task_id = fields.Many2one(
-        comodel_name='tsm.task', 
-        string='Task',
+        comodel_name="tsm.task",
+        string="Task",
         index=True,
     )
     project_id = fields.Many2one(
-        comodel_name='tsm.project', 
-        string='Project',
-        related='task_id.project_id',
+        comodel_name="tsm.project",
+        string="Project",
+        related="task_id.project_id",
         store=True,
-        index='btree_not_null',
+        index="btree_not_null",
     )
     user_id = fields.Many2one(
-        comodel_name="res.users", 
-        string='Assigned to',
+        comodel_name="res.users",
+        string="Assigned to",
         index=True,
-        default=lambda self: self.env.user, 
-        required=True
+        default=lambda self: self.env.user,
+        required=True,
     )
-    closed = fields.Boolean(related='task_id.stage_id.closed', readonly=True)
-    date_time_stop = fields.Datetime(compute='_get_stop_date_time',
-        string='End date time for calendar view', store=True, readonly=True)
-    task_partner_id = fields.Many2one(related='task_id.partner_id',
-        store=True, string='Customer')
-    tag_ids = fields.Many2one(
-        comodel_name='tsm.task.timesheet.tags', 
-        string='Timesheet Tags',
-        default=_get_default_tag_id,
+    closed = fields.Boolean(related="task_id.stage_id.closed", readonly=True)
+    date_time_stop = fields.Datetime(
+        compute="_get_stop_date_time",
+        string="End date time for calendar view",
+        store=True,
+        readonly=True,
+    )
+    task_partner_id = fields.Many2one(
+        related="task_id.partner_id", store=True, string="Customer"
     )
 
-    @api.depends('date_time', 'amount')
+    @api.depends("date_time", "amount")
     def _get_stop_date_time(self):
         for line in self:
             line.date_time_stop = datetime.strptime(
                 str(line.date_time), "%Y-%m-%d %H:%M:%S"
-                ) + timedelta(seconds=line.amount*3600)
+            ) + timedelta(seconds=line.amount * 3600)
 
     def button_end_work(self):
         end_date = fields.Datetime.now()
@@ -75,29 +98,31 @@ class TsmTaskTimesheet(models.Model):
         return True
 
     def button_open_task(self):
-        for line in self.filtered('task_id'):
-            stage = self.env['tsm.task.type'].search(
-                    [('closed', '=', False)], limit=1)
+        for line in self.filtered("task_id"):
+            stage = self.env["tsm.task.type"].search([("closed", "=", False)], limit=1)
             if stage:
-                line.task_id.write({'stage_id': stage.id})
+                line.task_id.write({"stage_id": stage.id})
 
     def button_close_task(self):
-        for line in self.filtered('task_id'):
-            stage = self.env['tsm.task.type'].search(
-                [('closed', '=', True)], limit=1,
+        for line in self.filtered("task_id"):
+            stage = self.env["tsm.task.type"].search(
+                [("closed", "=", True)],
+                limit=1,
             )
             if not stage:  # pragma: no cover
                 raise ValidationError(
-                    _("There isn't any stage with closed check. Please "
-                      "mark any.")
+                    _("There isn't any stage with closed check. Please " "mark any.")
                 )
             for t in line.task_id.timesheet_ids:
                 if t.amount == 0:
-                    raise ValidationError(_(
-                    "There are any timesheet with 00:00 hours in this task.\n"
-                    "That is not allowed in stages marked as closed."))
+                    raise ValidationError(
+                        _(
+                            "There are any timesheet with 00:00 hours in this task.\n"
+                            "That is not allowed in stages marked as closed."
+                        )
+                    )
 
-            line.task_id.write({'stage_id': stage.id})
+            line.task_id.write({"stage_id": stage.id})
 
     def toggle_closed(self):
         self.ensure_one()
