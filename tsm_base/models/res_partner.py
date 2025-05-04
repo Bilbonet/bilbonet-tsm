90af2844f80f50eb300b5aaa3be55c6acfee76a8
bilbonet <jesus@bilbonet.net>
2025-05-04 10:13:33 +0200

    pre-commit automatic changes

--- tsm_base/models/res_partner.py
+++ tsm_base/models/res_partner.py
@@ -4,37 +4,44 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    """ Inherits partner and adds Tasks information in the partner form """
-    _inherit = 'res.partner'
+    """Inherits partner and adds Tasks information in the partner form"""
 
-    tsm_task_ids = fields.One2many(string='TSM tasks',
-        comodel_name='tsm.task', 
-        inverse_name='partner_id',
-       )
+    _inherit = "res.partner"
+
+    tsm_task_ids = fields.One2many(
+        string="TSM tasks",
+        comodel_name="tsm.task",
+        inverse_name="partner_id",
+    )
     tsm_task_count = fields.Integer(
-        string='Amount Partner Tasks',
-        compute='_compute_tsm_task_count',
+        string="Amount Partner Tasks",
+        compute="_compute_tsm_task_count",
         readonly=True,
     )
 
     def _compute_tsm_task_count(self):
         # retrieve all children partners and prefetch 'parent_id' on them
-        all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-        all_partners.read(['parent_id'])
+        all_partners = self.with_context(active_test=False).search(
+            [("id", "child_of", self.ids)]
+        )
+        all_partners.read(["parent_id"])
 
-        tsm_task_groups = self.with_context(
-            active_test=False
-        ).env['tsm.task'].read_group(
-            domain=[('partner_id', 'in', all_partners.ids)],
-            fields=['partner_id'], groupby=['partner_id']
+        tsm_task_groups = (
+            self.with_context(active_test=False)
+            .env["tsm.task"]
+            .read_group(
+                domain=[("partner_id", "in", all_partners.ids)],
+                fields=["partner_id"],
+                groupby=["partner_id"],
+            )
         )
 
         partners = self.browse()
         for group in tsm_task_groups:
-            partner = self.browse(group['partner_id'][0])
+            partner = self.browse(group["partner_id"][0])
             while partner:
                 if partner in self:
-                    partner.tsm_task_count += group['partner_id_count']
+                    partner.tsm_task_count += group["partner_id_count"]
                     partners |= partner
                 partner = partner.parent_id
         (self - partners).tsm_task_count = 0
